c1e5c050a00a6677d33c2e952590d7f3cec63406
Rok Nemet <rok@roknemet.me>
2017-06-09 10:38:32 +0200

    Add proper DB seeders
    CategoriesSeeder initializes categories table

---
+++ database/seeds/CategoriesSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategoriesSeeder extends Seeder
+{
+
+    public function run()
+    {
+        $categories = collect(
+            json_decode(
+                file_get_contents(
+                    public_path('data/json/categories.json')
+                )
+            )
+        );
+
+        $categories->each( function ($iab) {
+
+            Category::create([
+                'code' => $iab->code,
+                'name' => $iab->type
+            ]);
+
+            sprintf('%s', "Created {$iab->code} \n");
+        });
+    }
+
+}
+
