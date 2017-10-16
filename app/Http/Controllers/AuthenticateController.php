6d71629090318f2eb21a46ef6e74b9de91869fac
Rok Nemet <rok@roknemet.me>
2017-10-16 12:02:37 +0200

    Add inactive user check
    Added additional authorization check. In case user is inactive login will be refused and an appropriate message will be sent back.

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -18,15 +18,18 @@ class AuthenticateController extends Controller
     public function authenticate()
     {
 
-        if (
-            !Auth::attempt([
-                'email'    => request('email'),
-                'password' => request('password')
-            ])
-        ) return $this->error(Response::HTTP_UNAUTHORIZED, 'Unauthorized', 'Wrong username / password');
+        if ( !Auth::attempt(
+            ['email'    => request('email'),
+             'password' => request('password')]
+        )) {
+            return $this->error(Response::HTTP_UNAUTHORIZED, 'Unauthorized', 'Wrong username / password');
+        }
 
-        return response()->json([
-            'token' => base64_encode(User::apiToken(Auth::user()))
-        ]);
+        if (Auth::user()->status != 1)
+            return $this->error(Response::HTTP_UNAUTHORIZED, 'Unauthorized', 'Your account is inactive. Please contact us at support@tapklik.com');
+
+        return response()->json(
+            ['token' => base64_encode(User::apiToken(Auth::user()))]
+        );
     }
 }
