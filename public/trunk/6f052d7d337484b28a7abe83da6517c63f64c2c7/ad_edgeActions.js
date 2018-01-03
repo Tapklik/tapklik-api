4c978144d0556dd38b41a1b6fcaf154d2bac6ce8
Rok Nemet <rok@roknemet.me>
2018-01-03 12:50:42 +0100

    Updated gitignore

--- public/trunk/6f052d7d337484b28a7abe83da6517c63f64c2c7/ad_edgeActions.js
+++
@@ -1,8 +0,0 @@
-
-(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
-//Edge symbol: 'stage'
-(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_clickTag}","click",function(sym,e){window.open(window.clickTag,'_blank');});
-//Edge binding end
-})("stage");
-//Edge symbol end:'stage'
-})(jQuery,AdobeEdge,"EDGE-100746699");
