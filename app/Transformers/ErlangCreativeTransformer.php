8c689752fc37faffd3aca3e48169275722b2efdb
Rok Nemet <rok@roknemet.me>
2018-04-05 10:13:21 +0200

    Add path node to erlang transformer

--- app/Transformers/ErlangCreativeTransformer.php
+++ app/Transformers/ErlangCreativeTransformer.php
@@ -27,22 +27,35 @@ class ErlangCreativeTransformer extends TransformerAbstract
             return '';
         }
 
-        return ['crid'       => $creative->uuid,
-                'class'      => $creative->class,
-                'type'       => $creative->type,
-                'h'          => $creative->h,
-                'w'          => $creative->w,
-                'attr'       => $creative->attr,
-                'expdir'     => $creative->expdir,
-                'pos'        => $creative->pos,
-                'ctrurl'     => $creative->ctrurl,
-                'iurl'       => $creative->iurl,
-                'adm'        => $creative->adm,
-                'adm_js'     => $creative->adm_js,
-                'adm_iframe' => $creative->adm_iframe,
-                'adm_url'    => $creative->adm_url,
-                'dim'        => [$creative->w.'x'.$creative->h],
-                'status'     => $creative->status,];
+        return [
+            'crid'       => $creative->uuid,
+            'class'      => $creative->class,
+            'type'       => $creative->type,
+            'h'          => $creative->h,
+            'w'          => $creative->w,
+            'attr'       => $creative->attr,
+            'expdir'     => $creative->expdir,
+            'pos'        => $creative->pos,
+            'ctrurl'     => $creative->ctrurl,
+            'iurl'       => $creative->iurl,
+            'adm'        => $creative->adm,
+            'adm_js'     => $creative->adm_js,
+            'adm_iframe' => $creative->adm_iframe,
+            'adm_url'    => $creative->adm_url,
+            'path'       => $this->_parseUrl($creative->iurl),
+            'dim'        => [$creative->w.'x'.$creative->h],
+            'status'     => $creative->status,
+	    ];
+    }
+
+    private function _parseUrl(string $url) {
+
+    	$url = parse_url($url);
+    	$path = explode('/', $url['path']);
+    	$path = array_pop($path);
+    	$path = implode('/', $path);
+
+    	return $url['scheme'] . '://' . $url['host'] . '/' . $path;
     }
 
     public function includeAttr(Creative $creative)
